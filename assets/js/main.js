a25ff1313c8cdedf7f5b48fc5173f4ffa2b328a3
Miguel Vasconcelos <mig.vasconcelos@outlook.com>
2025-06-18 15:14:30 +0100

    comments

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,24 @@
+//dar load a biblioteca das particulas no home page
 particlesJS.load("particles-js", "/assets/js/particle.js/particles_config.json");
 
+//Animação da barra de progresso ao abrir a página
 const bar = document.querySelector('.progress-bar');
 const value = bar.getAttribute('aria-valuenow');
 bar.style.setProperty('--target-width', value + '%');
 bar.style.width = 0;
 bar.classList.add('animate-progress');
 
+//Submissão do formulário de contacto com validação do captcha da cloudflare, o request é enviado via Fetch API
 const form = document.getElementById('contactForm');
-
 form.addEventListener('submit', async (e) => {
     e.preventDefault();
-
     const formData = new FormData(form);
-
     const turnstileResponse = document.querySelector('input[name="cf-turnstile-response"]')?.value;
-
     if (!turnstileResponse) {
         alert("Captcha não resolvido. Por favor tente novamente.");
         return;
     }
-
     formData.set("cf_turnstile_response", turnstileResponse);
-
     try {
         const response = await fetch("https://api.miguelvasconcelos.com/contact-site", {
             method: "POST",
@@ -43,6 +40,7 @@ form.addEventListener('submit', async (e) => {
     }
 });
 
+// Animação dos botões dos tabs com a bilbioteca Animate.css
 const tabs = document.querySelectorAll('.nav-link');
 let index = 0;
 
